d2a7d4f80c67f6dcf5f3e0871d90988dd787931a
sujcho <chosujin84@gmail.com>
2017-04-08 13:50:14 -0700

    CSS STYLE PARAGRAPH
    - style paragraph
    - font

--- JavaScript/Final_project/index.js
+++ JavaScript/Final_project/index.js
@@ -1,42 +1,72 @@
 window.onload = function(){
-    initialize();
+    addValidation();
 }
 
-function initialize(){
-    validateInput()
-}
-
-function validateInput(){
+function addValidation(){
     validateAmountOfLoan()
     validateAnnualInterest()
     validateRepaymentPeriod()
     validateZipcode()
 }
 function validateAmountOfLoan(){
-    validate("amountOfLoan", /\d/)
-}
-function validateAnnualInterest(){
-    validate("annualInterest", /\d/)
-}
-function validateRepaymentPeriod(){
-    validate("repaymentPeriod", /\d/)
-}
-function validateZipcode(){
-    validate("zipcode", /\d/)
-}
-function validate(element, testingForm){
-    var textbox = document.getElementById(element);
-    textbox.addEventListener("keypress", function(event){
-        //alert('key pressed' + String.fromCharCode(event.charCode ) + count_length)
-        var charCode = event.charCode;
-        if(charCode !=0){
-            if (!testingForm.test(String.fromCharCode(charCode))){
-                event.preventDefault();
+    validate("amountOfLoan", /\d/ , "msg1", function(event){
+        if(event.keyCode == 9){
+            if( this.value.length > 10){
+                var msgbox = document.getElementById("msg1");
+                msgbox.innerHTML="Number is too big";
             }
         }
     })
 }
+function validateAnnualInterest(){
+    validate("annualInterest", /\d/, "msg2", function(event){
+        if(event.keyCode == 9){
+            if( this.value > 100){
+                var msgbox = document.getElementById("msg2");
+                msgbox.innerHTML="Please type the correct interest";
+            }
+        }
+    })
+}
+function validateRepaymentPeriod(){
+    validate("repaymentPeriod", /\d/, "msg3",function(event){
+        if(event.keyCode == 9){
+            if( this.value > 50){
+                var msgbox = document.getElementById("msg3");
+                msgbox.innerHTML="Please type the correct period: max.50";
+            }
+        }
+    })
+}
+function validateZipcode(){
+    validate("zipcode", /\d/, "msg4", function(event){
+        if(event.keyCode == 9){
+            if( this.value.length > 5){
+                var msgbox = document.getElementById("msg4");
+                msgbox.innerHTML="Please type the correct zip code: 5 digit";
+            }
+        }
+    })
+}
+function validate(element, testingForm ,msg, moreCheck){
+    var textbox = document.getElementById(element);
+    textbox.addEventListener("keypress", function(event){
 
+        //alert('key pressed' + String.fromCharCode(event.charCode ) + count_length)
+        var charCode = event.charCode;
+        var msgbox = document.getElementById(msg);
+        msgbox.innerHTML="";
+        var msgbox2 = document.getElementById("msg5");
+        msgbox2.innerHTML = ""
+        if(charCode !=0){
+            if (!testingForm.test(String.fromCharCode(charCode))){
+                event.preventDefault();
+                msgbox.innerHTML = "Please enter the number."
+            }
+        }
+    })
+    textbox.addEventListener("keydown", moreCheck);
+}
 
 /*
  * This script defines the calculate() function called by the event handlers
@@ -46,11 +76,16 @@ function validate(element, testingForm){
  */
 function calculate(){
 
-    alert("onclick")
-
     var initial = document.getElementById("amountOfLoan").value;
-    var annual_i = document.getElementsById("annualInterest").value;
+    var annual_i = document.getElementById("annualInterest").value;
     var period = document.getElementById("repaymentPeriod").value;
+
+    //if anything is null, message error
+    if(!initial||!annual_i||!period){
+        var msgbox = document.getElementById("msg5");
+        msgbox.innerHTML = "Please type all values";
+        return
+    }
 
     //monthly interest
     var i = (annual_i * 0.01)/12;
@@ -58,17 +93,74 @@ function calculate(){
     //total number of payment
     var n = period * 12
 
-    var month_payment = initial * i * (Math.pow(1+ i, n)) / (Math.pow(1 + i, n) - 1);
-    var total_payment = month_payment * n;
-    var total_interest = total_payment - initial;
-
+    var month_payment = Math.round(initial * i * (Math.pow(1+ i, n)) / (Math.pow(1 + i, n) - 1));
+    var total_payment = Math.round(month_payment * n);
+    var total_interest = Math.round(total_payment - initial);
 
     document.getElementById("month_payment").innerHTML = ""+month_payment
+    document.getElementById("total_payment").innerHTML = total_payment
+    document.getElementById("total_interest").innerHTML = total_interest
 
-
+    getLenders(initial, annual_i,period)
+    chart(initial, total_interest, month_payment, total_payment)
 
 }
 
 
+function getLenders(amount, apr, years, zipcode){
+
+    var user_data ={}
+    user_data.amount = amount
+    user_data.apr = apr
+    user_data.years = years
+
+    var user_json = JSON.stringify(user_data);
+
+    var request;
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    request.open('GET', 'data.json');
+    request.onreadystatechange = function() {
+        if ((request.readyState===4) && (request.status===200)) {
+            var items = JSON.parse(request.responseText);
+            var output = '<ul>';
+            for (var key in items) {
+                output += '<li>' + items[key].name + '<a href=' + items[key].link + '>' + items[key].link + '</a>' +'</li>';
+            }
+            output += '</ul>';
+            document.getElementById('landers').innerHTML = output;
+        }
+    };
+    request.send();
+}
+
+function chart(principal, interest, monthly, payments) {
+    var canvas = document.getElementById('chart');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        //ctx.fillRect(x, y, width, height);
+        //normalize
+        //principal
+        ctx.fillStyle = 'green';
+        ctx.fillRect(0, 100, 70, 200);
+
+        height = (interest/principal) * 200;
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(80, 100, 70, 100);
 
 
+        height = (monthly/principal) * 200;
+        ctx.fillStyle = 'red';
+        ctx.fillRect(160, 100, 70, height);
+
+        height = (payments/principal) * 200;
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(240, 100, 70, height);
+
+
+    }
+}
